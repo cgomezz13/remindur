f349812dade2025509b13b4ea0284440688a875f
Cindy <c.puchex@gmail.com>
2018-05-18 00:26:01 -0400

    search bar working on frontend

--- frontend/components/user_homepage/homepage.jsx
+++ frontend/components/user_homepage/homepage.jsx
@@ -1,20 +1,70 @@
 import React from 'react';
 import GearDropdown from './dropdown'
 
-const user_homepage = (props) => {
 
-  return (
-    <header className='user-page-navbar'>
+class userHomepage extends React.Component {
+  constructor (props) {
+    super(props);
 
-      <section className='user-nav-left'>
-        search here
-      </section>
-
-
-      <GearDropdown user={props.current_user} logout={props.logout}/>
+    this.state = {query: '', results: []};
+    this.handleSearch = this.handleSearch.bind(this);
+  }
 
 
-    </header>
-  )
+  handleSearch() {
+    return e => {
+      const query = e.target.value
+      let newResults = Object.assign([], this.state.results);
+
+      this.props.tasks.forEach(task => {
+        if (task.body.toLowerCase().slice(0, query.length) === query && !newResults.includes(task.body)) {
+          newResults.push(task.body);
+        }
+      })
+
+      newResults.forEach(result => {
+        if (result.toLowerCase().slice(0, query.length) !== query) {
+          const index = newResults.indexOf(result)
+          newResults.splice(index, 1);
+        }
+      })
+
+      if (query === '') {
+        newResults = [];
+      }
+      this.setState({query: query});
+      this.setState({results: newResults})
+    };
+  }
+
+
+
+  render () {
+    console.log(this.state.results);
+    const searchResults = this.state.results.map((body, idx) => {
+      return (
+        <li key={idx}>{body}</li>
+      )
+    })
+
+    console.log(searchResults)
+
+    return (
+      <header className='user-page-navbar'>
+
+        <section className='user-nav-left'>
+          <input onChange={this.handleSearch()} type='text' placeholder= 'Search...' value={this.state.query}></input>
+          <ul>
+            {searchResults}
+          </ul>
+        </section>
+
+
+        <GearDropdown user={this.props.current_user} logout={this.props.logout}/>
+
+
+      </header>
+    )
+  }
 }
-export default user_homepage;
+export default userHomepage;
--- frontend/components/user_homepage/user_homepage_container.jsx
+++ frontend/components/user_homepage/user_homepage_container.jsx
@@ -2,9 +2,30 @@ import { connect } from 'react-redux';
 import Homepage from './homepage';
 import { logout } from '../../actions/session_actions';
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+  let tasks, taskIds;
+  if (ownProps.location.pathname === '/tasks') {
+    if (jQuery.isEmptyObject(state.tasks)) {
+      tasks = [];
+    } else {
+      tasks = Object.values(state.tasks);
+    }
+  } else {
+    const listUrl = ownProps.location.pathname.split('/');
+    const listId = +listUrl[2];
+    if (jQuery.isEmptyObject(state.lists)) {
+      taskIds = []
+    } else {
+      taskIds = state.lists[listId].task_ids;
+    }
+    tasks = taskIds.map(id => {
+      return (state.tasks[id] || {} )
+    })
+  }
+
   return ({
-    current_user: state.session.user.username
+    current_user: state.session.user.username,
+    tasks: tasks
   })
 }
 
