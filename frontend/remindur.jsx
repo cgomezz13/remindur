b678b290de3d9b39ef33c539ec62c5472221d4f1
Cindy <c.puchex@gmail.com>
2018-05-28 10:46:24 -0400

    removing functions on window

--- frontend/remindur.jsx
+++ frontend/remindur.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { logout } from './actions/session_actions';
-
-import { createTask, deleteTask, updateTask } from './actions/task_actions';
-import { fetchAllLists, createNewList, updateList, deleteList } from './actions/list_actions';
-
 document.addEventListener("DOMContentLoaded", () => {
   let store;
 
@@ -20,17 +15,6 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-
-  window.store = store;
-  window.logout = logout;
-  window.createTask = createTask;
-  window.deleteTask = deleteTask;
-  window.updateTask = updateTask;
-  window.fetchAllLists = fetchAllLists;
-  window.createNewList = createNewList;
-  window.updateList = updateList;
-  window.deleteList = deleteList;
-
 
   ReactDOM.render(<Root store={store}/>, document.getElementById("root"))
 })
