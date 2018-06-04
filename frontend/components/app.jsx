a2042c3a8926a190974346cf7269d207500ca7e3
Cindy <c.puchex@gmail.com>
2018-06-04 15:22:42 -0400

    editing for more respsonsive styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import SessionFormContainer from './session_form/session_form_container';
-import Welcome from './welcomepage/welcome';
-import { Route, Switch } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import TaskFormContainer from './task_form/all_tasks_container';
-import UserHomepageContainer from './user_homepage/user_homepage_container';
-import ListFormContainer from './lists_form/list_form_container';
-import ListTaskContainer from './task_form/list_tasks_container';
-import EditTaskContainer from './edit_task/edit_task_container';
+import React from "react";
+import { Provider } from "react-redux";
+import SessionFormContainer from "./session_form/session_form_container";
+import Welcome from "./welcomepage/welcome";
+import { Route, Switch } from "react-router-dom";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import TaskFormContainer from "./task_form/all_tasks_container";
+import UserHomepageContainer from "./user_homepage/user_homepage_container";
+import ListFormContainer from "./lists_form/list_form_container";
+import ListTaskContainer from "./task_form/list_tasks_container";
+import EditTaskContainer from "./edit_task/edit_task_container";
 
 const App = () => (
   <div>
-      <Switch>
-        <Route exact path='/welcome' component={Welcome} />
-        <AuthRoute exact path='/login' component={SessionFormContainer} />
-        <AuthRoute exact path='/signup' component={SessionFormContainer} />
-      </Switch>
-      <ProtectedRoute path='/' component={UserHomepageContainer} />
-      <ProtectedRoute path='/' component={ListFormContainer} />
-      <ProtectedRoute path='/tasks' component={TaskFormContainer} />
-      <ProtectedRoute path='/lists/:listId/tasks' component={ListTaskContainer} />
-      <ProtectedRoute path='/tasks/:taskId/edit' component={EditTaskContainer} />
-      <ProtectedRoute path='/lists/:listId/tasks/:taskId/edit' component={EditTaskContainer} />
+    <Switch>
+      <Route exact path="/welcome" component={Welcome} />
+      <AuthRoute exact path="/login" component={SessionFormContainer} />
+      <AuthRoute exact path="/signup" component={SessionFormContainer} />
+    </Switch>
+    <ProtectedRoute path="/" component={UserHomepageContainer} />
+    <div className="list-and-task">
+      <ProtectedRoute path="/" component={ListFormContainer} />
+      <div className="task-show">
+        <ProtectedRoute path="/tasks" component={TaskFormContainer} />
+        <ProtectedRoute
+          path="/lists/:listId/tasks"
+          component={ListTaskContainer}
+        />
+        <ProtectedRoute
+          path="/tasks/:taskId/edit"
+          component={EditTaskContainer}
+        />
+        <ProtectedRoute
+          path="/lists/:listId/tasks/:taskId/edit"
+          component={EditTaskContainer}
+        />
+      </div>
+    </div>
   </div>
 );
 
--- frontend/components/task_form/task_form.jsx
+++ frontend/components/task_form/task_form.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
+import React from "react";
+import { Redirect } from "react-router-dom";
 
 class taskForm extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
-    this.state = {body: '', due_date: '', status: '', note: '', list_id: '', selectedTaskIds: [] };
+    this.state = {
+      body: "",
+      due_date: "",
+      status: "",
+      note: "",
+      list_id: "",
+      selectedTaskIds: []
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.selectionAction = this.selectionAction.bind(this);
     this.handleDeleteAll = this.handleDeleteAll.bind(this);
   }
 
-  update(type){
+  update(type) {
     return e => {
-      this.setState({[type]: e.target.value});
+      this.setState({ [type]: e.target.value });
     };
   }
 
@@ -21,60 +28,63 @@ class taskForm extends React.Component {
     this.props.action(this.props.match.params.listId);
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     return e => {
       e.preventDefault();
       const newTask = Object.assign({}, this.state);
-      if (this.props.formType === 'list_task') {
-        Object.assign(newTask, {list_id: this.props.match.params.listId});
+      if (this.props.formType === "list_task") {
+        Object.assign(newTask, { list_id: this.props.match.params.listId });
       }
-      this.props.createTask(newTask).then(() => this.setState({body: ''}));
+      this.props.createTask(newTask).then(() => this.setState({ body: "" }));
     };
   }
 
-  selectionAction (id) {
-    const selectedIds = Object.assign([], this.state.selectedTaskIds );
+  selectionAction(id) {
+    const selectedIds = Object.assign([], this.state.selectedTaskIds);
     const last = this.props.match.url;
 
     if (selectedIds.includes(id)) {
       const idx = selectedIds.indexOf(id);
       selectedIds.splice(idx, 1);
-      this.setState({selectedTaskIds: selectedIds});
+      this.setState({ selectedTaskIds: selectedIds });
     } else {
       selectedIds.push(id);
-      this.setState({selectedTaskIds: selectedIds});
+      this.setState({ selectedTaskIds: selectedIds });
     }
 
     if (selectedIds.length === 1) {
       const taskId = selectedIds[0];
-      const path = last +  '/' + taskId + '/edit';
-      this.props.history.push(path)
+      const path = last + "/" + taskId + "/edit";
+      this.props.history.push(path);
     } else {
       this.props.history.push(last);
     }
-
   }
 
-  isChecked (id) {
-    this.state.selectedTaskIds.includes(id)
+  isChecked(id) {
+    this.state.selectedTaskIds.includes(id);
   }
 
-  handleDeleteAll () {
+  handleDeleteAll() {
     this.state.selectedTaskIds.forEach(id => {
       this.props.deleteTask(id).then();
-    })
-    this.setState({selectedTaskIds: []})
+    });
+    this.setState({ selectedTaskIds: [] });
   }
 
-
-  render () {
+  render() {
     const allTasks = this.props.tasks.map(task => {
       return (
         <label key={task.id} className="Lifazul">
-          <input onClick={() => this.selectionAction(task.id)} checked={this.isChecked(task.id)} type='checkbox' name='selection' />
+          <input
+            onClick={() => this.selectionAction(task.id)}
+            checked={this.isChecked(task.id)}
+            type="checkbox"
+            name="selection"
+          />
           <span>{task.body}</span>
         </label>
-      )
+      );
     });
 
     const status = this.props.tasks;
@@ -82,54 +92,72 @@ class taskForm extends React.Component {
     let TotalTasks = 0;
 
     status.forEach(task => {
-      TotalTasks++ ;
-      if (task.status) { NumofCompleted++; }
-    })
+      TotalTasks++;
+      if (task.status) {
+        NumofCompleted++;
+      }
+    });
 
     let deleteAllOption;
 
     if (this.state.selectedTaskIds.length > 1) {
       deleteAllOption = (
-        <button className='deleteAll' onClick={() => this.handleDeleteAll()}>Delete All Selected Tasks</button>
-      )
+        <button className="deleteAll" onClick={() => this.handleDeleteAll()}>
+          Delete All Selected Tasks
+        </button>
+      );
     } else {
-      deleteAllOption = '';
+      deleteAllOption = "";
     }
 
-
     return (
-
-        <section>
-        <section className='main-form'>
+      <section className="task-default-show">
+        <section className="main-form">
           <h1>Tasks</h1>
-          <form onSubmit={this.handleSubmit()} className='task-form'>
-            <input onChange={this.update('body')} type='text' value={this.state.body} placeholder='Add a Task'/>
-            <input type='submit' value='Add Task' />
+          <form onSubmit={this.handleSubmit()} className="task-form">
+            <input
+              onChange={this.update("body")}
+              type="text"
+              value={this.state.body}
+              placeholder="Add a Task"
+            />
+            <input type="submit" value="Add Task" />
           </form>
-          <section className='list-items'>
+          <section className="list-items">
             <ul>{allTasks}</ul>
-          </section>
-          </section>
-
-          <section className={(this.state.selectedTaskIds.length === 0) ? 'visible-list-summary' : 'hidden-list-summary'}>
-            <ul>
-              <label>
-                <li>{TotalTasks}</li>
-                Tasks
-              </label>
-              <label>
-                <li>{NumofCompleted}</li>
-                Completed
-              </label>
-            </ul>
-          </section>
-
-          <section className={(this.state.selectedTaskIds.length > 1) ? 'visible-delete-option' : 'hidden-delete-option'}>
-            {deleteAllOption}
           </section>
         </section>
 
-    )
+        <section
+          className={
+            this.state.selectedTaskIds.length === 0
+              ? "visible-list-summary"
+              : "hidden-list-summary"
+          }
+        >
+          <ul>
+            <label>
+              <li>{TotalTasks}</li>
+              Tasks
+            </label>
+            <label>
+              <li>{NumofCompleted}</li>
+              Completed
+            </label>
+          </ul>
+        </section>
+
+        <section
+          className={
+            this.state.selectedTaskIds.length > 1
+              ? "visible-delete-option"
+              : "hidden-delete-option"
+          }
+        >
+          {deleteAllOption}
+        </section>
+      </section>
+    );
   }
 }
 
