abf327f2e3b70aaf82c15981cd90aa6512efe6ec
Cindy <c.puchex@gmail.com>
2018-06-18 22:28:57 -0400

    fixed font weight of modal

--- frontend/components/lists_form/list_form.jsx
+++ frontend/components/lists_form/list_form.jsx
@@ -1,86 +1,97 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import EditListDropdown from './edit_list_dropdown';
+import React from "react";
+import { Link } from "react-router-dom";
+import EditListDropdown from "./edit_list_dropdown";
 
 class ListForm extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
-    this.state = {list_title: '', visible: false}
+    this.state = { list_title: "", visible: false };
     this.changeVisibility = this.changeVisibility.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.props.fetchAllLists();
   }
 
-  changeVisibility () {
-    this.setState({visible: !this.state.visible });
+  changeVisibility() {
+    this.setState({ visible: !this.state.visible });
   }
 
   update(type) {
     return e => {
-      this.setState({[type]: e.target.value});
+      this.setState({ [type]: e.target.value });
     };
   }
 
-  handleSubmit (e) {
+  handleSubmit(e) {
     e.preventDefault();
-    const list = Object.assign({}, {list_title: this.state.list_title});
+    const list = Object.assign({}, { list_title: this.state.list_title });
     this.changeVisibility();
-    this.setState({['list_title']: ''});
+    this.setState({ ["list_title"]: "" });
     this.props.createNewList(list);
   }
 
-  render () {
+  render() {
     const allLists = this.props.lists.map(list => {
       return (
         <li key={list.id}>
-          <Link className='list-link' to={`/lists/${list.id}/tasks`}>{list.list_title}</Link>
-          <EditListDropdown list={list} update={this.props.updateList} delete={this.props.deleteList} />
+          <Link className="list-link" to={`/lists/${list.id}/tasks`}>
+            {list.list_title}
+          </Link>
+          <EditListDropdown
+            list={list}
+            update={this.props.updateList}
+            delete={this.props.deleteList}
+          />
         </li>
-      )
-    })
-
+      );
+    });
 
     return (
-      <section className='main-siderbar'>
+      <section className="main-siderbar">
         <section>
           <h2>T A S K S</h2>
           <li>
-            <Link to={'/tasks'}>All Tasks</Link>
+            <Link to={"/tasks"}>All Tasks</Link>
           </li>
         </section>
 
-
-        <header className='list-titles'>
-
+        <header className="list-titles">
           <h2>L I S T S </h2>
 
-          <button id='list-btn' onClick={()=>this.changeVisibility()}><i className="fas fa-plus"></i></button>
-
-          <div id='myModal' className={this.state.visible ? 'visible-list-modal' : 'hidden-list-modal'}>
-            <div className='modal-content'>
-              <span onClick={()=>this.changeVisibility()} className="close">&times;</span>
-              <h1>Add a List</h1>
-              <form onSubmit={this.handleSubmit}>
-                <label>Please enter a list name:</label>
-                <input onChange={this.update('list_title')} type='text' value={this.state.list_title}></input>
-                <input type='submit' value='Add'></input>
-              </form>
-            </div>
-          </div>
-
+          <button id="list-btn" onClick={() => this.changeVisibility()}>
+            <i className="fas fa-plus" />
+          </button>
         </header>
 
+        <div
+          id="myModal"
+          className={
+            this.state.visible ? "visible-list-modal" : "hidden-list-modal"
+          }
+        >
+          <div className="modal-content">
+            <span onClick={() => this.changeVisibility()} className="close">
+              &times;
+            </span>
+            <h1>Add a List</h1>
+            <form onSubmit={this.handleSubmit}>
+              <label>Please enter a list name:</label>
+              <input
+                onChange={this.update("list_title")}
+                type="text"
+                value={this.state.list_title}
+              />
+              <input type="submit" value="Add" />
+            </form>
+          </div>
+        </div>
 
-
-
-        <ul className='list-titles'>{allLists}</ul>
+        <ul className="list-titles">{allLists}</ul>
       </section>
-    )
-
+    );
   }
 }
 
