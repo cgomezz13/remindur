d71a186b4efc4d821a812e56cedb20ef9b83340b
Cindy <c.puchex@gmail.com>
2018-07-23 14:38:19 -0400

    changed error for mapStateToProp

--- frontend/components/session_form/session_form_container.jsx
+++ frontend/components/session_form/session_form_container.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { signUp, login } from '../../actions/session_actions';
-import SessionForm from './session_form';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { signUp, login } from "../../actions/session_actions";
+import SessionForm from "./session_form";
 
 const mapStateToProps = (state, ownProps) => {
   return {
     formType: ownProps.location.pathname.slice(1),
     errors: state.errors.sessionErrors,
-    demo: {username: 'guest', password: 'password'}
+    demo: { username: "guest", password: "password" }
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-
-  if (ownProps.location.pathname.slice(1) === 'signup') {
-    return({
-      action: (user) => dispatch(signUp(user))
-    });
+  if (ownProps.location.pathname.slice(1) === "signup") {
+    return {
+      action: user => dispatch(signUp(user))
+    };
   } else {
-    return({
-      action: (user) => dispatch(login(user))
-    });
+    return {
+      action: user => dispatch(login(user))
+    };
   }
-
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
