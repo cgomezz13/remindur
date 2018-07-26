ab91c3ecc39ac300934d47525fc2fc1a807f5421
Cindy <c.puchex@gmail.com>
2018-07-26 14:10:02 -0400

    creating reducer to listen to errors when creating/updating list

---
+++ frontend/reducers/list_errors_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_LIST_ERRORS, UPDATE_LIST } from "../actions/list_actions";
+
+const listErrorsReducer = (state = [], action) => {
+  Object.freeze(state);
+  debugger;
+  switch (action.type) {
+    case RECEIVE_LIST_ERRORS:
+      return action.errors;
+    case UPDATE_LIST:
+      return action.errors;
+    default:
+      return state;
+  }
+};
+
+export default listErrorsReducer;
