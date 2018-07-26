7d667020e1974fde6e4c73711113bc3ab0d0a1e6
Cindy <c.puchex@gmail.com>
2018-07-26 16:25:19 -0400

    adding recieve list error action

--- frontend/actions/list_actions.js
+++ frontend/actions/list_actions.js
@@ -1,87 +1,103 @@
-import React from 'react';
-import * as ListAPIUtil from '../util/list_api_util';
+import React from "react";
+import * as ListAPIUtil from "../util/list_api_util";
 
-export const FETCH_ALL_LISTS = 'FETCH_ALL_LISTS';
-export const CREATE_NEW_LIST = 'CREATE_NEW_LIST';
-export const UPDATE_LIST = 'UPDATE_LIST';
-export const FETCH_A_LIST = 'FETCH_A_LIST';
-export const DELETE_LIST = 'DELETE_LIST';
+export const FETCH_ALL_LISTS = "FETCH_ALL_LISTS";
+export const CREATE_NEW_LIST = "CREATE_NEW_LIST";
+export const UPDATE_LIST = "UPDATE_LIST";
+export const FETCH_A_LIST = "FETCH_A_LIST";
+export const DELETE_LIST = "DELETE_LIST";
+export const RECEIVE_LIST_ERRORS = "RECEIVE_LIST_ERRORS";
 
-
-export const GetAllLists = (lists) => {
+export const GetAllLists = lists => {
   return {
     type: FETCH_ALL_LISTS,
     lists
-  }
-}
+  };
+};
 
-export const createList = (list) => {
-  return ({
+export const createList = list => {
+  return {
     type: CREATE_NEW_LIST,
     list
-  })
-}
+  };
+};
 
-
-export const updateAList = (list) => {
-  return({
+export const updateAList = list => {
+  return {
     type: UPDATE_LIST,
     list
-  })
-}
+  };
+};
 
 const fetchAList = ({ list, tasks }) => {
-  return ({
+  return {
     type: FETCH_A_LIST,
     list,
     tasks
-  })
-}
+  };
+};
 
-const removelist = ({listId, taskIds}) => {
-  return ({
+const removelist = ({ listId, taskIds }) => {
+  return {
     type: DELETE_LIST,
     listId,
     taskIds
-  })
-}
+  };
+};
+
+const receiveErrors = errors => {
+  return {
+    type: RECEIVE_LIST_ERRORS,
+    errors
+  };
+};
 
 export const fetchAllLists = () => {
   return dispatch => {
     return ListAPIUtil.fetchAllLists().then(lists => {
-      return (dispatch(GetAllLists(lists)));
-    })
-  }
-}
-
-export const createNewList = (list) => {
-  return dispatch => {
-    return ListAPIUtil.createList(list).then(list => {
-      return dispatch(createList(list));
-    })
-  }
-}
-
-export const updateList = (list) => {
-  return dispatch => {
-    return ListAPIUtil.updateList(list).then(list=>{
-      return dispatch(updateAList(list));
+      return dispatch(GetAllLists(lists));
     });
   };
 };
 
-export const deleteList = (id) => {
+export const createNewList = list => {
   return dispatch => {
-    return ListAPIUtil.deleteList(id).then(({task_ids: taskIds}) => {
-      return dispatch(removelist({taskIds, listId: id}))
+    return ListAPIUtil.createList(list).then(
+      list => {
+        return dispatch(createList(list));
+      },
+      errors => {
+        return dispatch(receiveErrors(errors.responseJSON));
+      }
+    );
+  };
+};
+
+export const updateList = list => {
+  return dispatch => {
+    return ListAPIUtil.updateList(list).then(
+      list => {
+        return dispatch(updateAList(list));
+      },
+      errors => {
+        return dispatch(receiveErrors(errors.responseJSON));
+      }
+    );
+  };
+};
+
+export const deleteList = id => {
+  return dispatch => {
+    return ListAPIUtil.deleteList(id).then(({ task_ids: taskIds }) => {
+      return dispatch(removelist({ taskIds, listId: id }));
     });
   };
 };
 
-export const fetchList = (id) => {
+export const fetchList = id => {
   return dispatch => {
     return ListAPIUtil.fetchListsTask(id).then(list => {
-      return dispatch(fetchAList(list))
+      return dispatch(fetchAList(list));
     });
   };
 };
