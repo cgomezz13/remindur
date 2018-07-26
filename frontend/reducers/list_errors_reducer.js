b777c7b43133f8b8f4c3611f179b107de33edbb7
Cindy <c.puchex@gmail.com>
2018-07-26 17:43:35 -0400

    removing debugger

--- frontend/reducers/list_errors_reducer.js
+++ frontend/reducers/list_errors_reducer.js
@@ -2,7 +2,6 @@ import { RECEIVE_LIST_ERRORS, UPDATE_LIST } from "../actions/list_actions";
 
 const listErrorsReducer = (state = [], action) => {
   Object.freeze(state);
-  debugger;
   switch (action.type) {
     case RECEIVE_LIST_ERRORS:
       return action.errors;
