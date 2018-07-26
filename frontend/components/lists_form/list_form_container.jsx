a92a33903e716228c1f09518bde2c497f1bb869a
Cindy <c.puchex@gmail.com>
2018-07-26 17:39:54 -0400

    adding listErrors to mapStateToProps

--- frontend/components/lists_form/list_form_container.jsx
+++ frontend/components/lists_form/list_form_container.jsx
@@ -1,23 +1,28 @@
-import { connect } from 'react-redux';
-import { deleteList, createNewList, updateList, fetchAllLists } from '../../actions/list_actions';
-import { allTasks } from '../../actions/task_actions';
-import ListForm from './list_form';
+import { connect } from "react-redux";
+import {
+  deleteList,
+  createNewList,
+  updateList,
+  fetchAllLists
+} from "../../actions/list_actions";
+import { allTasks } from "../../actions/task_actions";
+import ListForm from "./list_form";
 
-
-const mapStateToProps = (state) => {
-  return ({
-    lists: Object.values(state.lists)
-  })
+const mapStateToProps = state => {
+  return {
+    lists: Object.values(state.lists),
+    errors: state.errors.listErrors
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return ({
-    deleteList: (id) => dispatch(deleteList(id)),
-    createNewList: (list) => dispatch(createNewList(list)),
-    updateList: (list) => dispatch(updateList(list)),
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteList: id => dispatch(deleteList(id)),
+    createNewList: list => dispatch(createNewList(list)),
+    updateList: list => dispatch(updateList(list)),
     fetchAllLists: () => dispatch(fetchAllLists()),
     fetchAllTasks: () => dispatch(allTasks())
-  })
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ListForm);
