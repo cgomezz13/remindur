f866dc88bb1edeb0e2d570ded2b07d69f4d7b0be
Cindy <c.puchex@gmail.com>
2018-07-26 16:25:46 -0400

    adding listError to errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,8 +1,10 @@
-import sessionErrorsReducer from './session_errors_reducer';
-import { combineReducers } from 'redux';
+import sessionErrorsReducer from "./session_errors_reducer";
+import listErrorsReducer from "./list_errors_reducer";
+import { combineReducers } from "redux";
 
 const errorsReducer = combineReducers({
-    sessionErrors: sessionErrorsReducer
-})
+  sessionErrors: sessionErrorsReducer,
+  listErrors: listErrorsReducer
+});
 
 export default errorsReducer;
