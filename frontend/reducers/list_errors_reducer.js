d39720d74da2633e3f1a22f006a00a6bc1cdd180
Cindy <c.puchex@gmail.com>
2018-07-27 16:40:36 -0400

    adding to list Errors reducer, clear errors when new list is successfully created

--- frontend/reducers/list_errors_reducer.js
+++ frontend/reducers/list_errors_reducer.js
@@ -1,12 +1,13 @@
-import { RECEIVE_LIST_ERRORS, UPDATE_LIST } from "../actions/list_actions";
+import { RECEIVE_LIST_ERRORS, CREATE_NEW_LIST } from "../actions/list_actions";
 
 const listErrorsReducer = (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_LIST_ERRORS:
       return action.errors;
-    case UPDATE_LIST:
-      return action.errors;
+    case CREATE_NEW_LIST:
+      const newState = [];
+      return newState;
     default:
       return state;
   }
