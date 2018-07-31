e50d4aa9f19350d8f151a3f2e28510fc0f41c886
Cindy <c.puchex@gmail.com>
2018-07-31 17:07:48 -0400

    adding a clear list errors action to reducer

--- frontend/reducers/list_errors_reducer.js
+++ frontend/reducers/list_errors_reducer.js
@@ -1,13 +1,19 @@
-import { RECEIVE_LIST_ERRORS, CREATE_NEW_LIST } from "../actions/list_actions";
+import {
+  RECEIVE_LIST_ERRORS,
+  CREATE_NEW_LIST,
+  CLEAR_LIST_ERRORS
+} from "../actions/list_actions";
 
 const listErrorsReducer = (state = [], action) => {
+  const noErrors = [];
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_LIST_ERRORS:
       return action.errors;
     case CREATE_NEW_LIST:
-      const newState = [];
-      return newState;
+      return noErrors;
+    case CLEAR_LIST_ERRORS:
+      return noErrors;
     default:
       return state;
   }
